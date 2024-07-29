efc207ff94e43773f77711b4ab55238a97cb2259
Lucas Santos <Luckas233@hotmail.com.br>
2024-07-29 15:13:58 -0300

    Adicionando comentários e um SupressWarning para facilitação de leitura.

--- src/Contador.java
+++ src/Contador.java
@@ -8,6 +8,8 @@ class ParametrosInvalidosException extends Exception {
 
 public class Contador {
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        //SupressWarnings utilizado para retirar o aviso sobre o Scanner terminal, pois estava sendo considerado um erro.
         Scanner terminal = new Scanner(System.in);
         System.out.println("Digite o primeiro parâmetro:");
         int parametroUm = terminal.nextInt();
@@ -15,18 +17,23 @@ public class Contador {
         int parametroDois = terminal.nextInt();
 
         try {
+            //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
         } catch (ParametrosInvalidosException e) {
+            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
             System.out.println(e.getMessage());
         }
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        	//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+
         if (parametroUm >= parametroDois) {
             throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
         }
-
+        
         for (int i = parametroUm; i <= parametroDois; i++) {
+            //realizar o for para imprimir os números com base na variável contagem
             System.out.println("Imprimindo o número " + i);
         }
     }
